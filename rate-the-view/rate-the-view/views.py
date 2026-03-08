7c67d99f06b5cfbd97f217e832dc9ca122603896
Mohammed Alkhulaifi <3075556A@student.gla.ac.uk>
2026-03-08 16:38:24 +0000

    changees to view.py  and add initial HTML template for the homepage

--- rate-the-view/rate-the-view/views.py
+++ rate-the-view/rate-the-view/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
 
 def home(request):
-    context_dict = {}
-    return render(request, 'rate-the-veiw/home.html', context=context_dict)
+    return render(request, 'rate-the-view/home.html')
