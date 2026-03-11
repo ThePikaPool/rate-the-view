bfc977dc0faf5b5e111c6a3635a14d7dd089daa4
Mohammed Alkhulaifi <3075556A@student.gla.ac.uk>
2026-03-11 18:00:05 +0000

    Fixed home.html instead of being hardcoded

--- rate-the-view/rate_the_view/views.py
+++ rate-the-view/rate_the_view/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'rate_the_view/home.html')
+
+    # Example placeholder data for homepage posts
+    # This is temporary so the template can render
+    # Teammate who implements the database should replace this
+    posts = [
+        {
+            "username": "callum3925",
+            "location": "Isle of Skye",
+            "title": "Sunset at Isle of Skye",
+            "votes": 82
+        }
+    ]
+
+    context = {"posts": posts}
+
+    return render(request, 'rate_the_view/home.html', context)
